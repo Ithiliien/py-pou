57e451bcee6c12846212caa40b25794acba63042
María Álvarez Jiménez <maj20091999@gmail.com>
2024-01-10 13:40:01 +0100

    pou con estructura funcional

--- pou+.py
+++ pou+.py
@@ -1,4 +1,5 @@
 import random
+
 def minmax(value):
   if value < 0:
     return 0
@@ -7,16 +8,16 @@ def minmax(value):
   else:
     return value
 
-  state ={
-     "name": "Toto",
-     "age": 0,
-     "hunger": random.randint(0,50),
-     "energy": random.randint(50,100),
-     "happiness": 100,
-     "health": 100,
-     "alive": True
+state ={
+  "name": "Toto",
+   "age": 0,
+   "hunger": random.randint(0,50),
+   "energy": random.randint(50,100),
+   "happiness": 100,
+   "health": 100,
+   "alive": True
   }
-  def status():
+def status():
      print("Name:" ,  state["name"])
      print("Age:" ,  state["age"])
      print("Hunger:" ,  state["hunger"])
@@ -24,7 +25,7 @@ def minmax(value):
      print("Happiness:" ,  state["happiness"])
      print("Health" ,  state["health"])
   
-  def play(state):
+def play(state):
      new_state= dict(state)
      new_state["hunger"]= minmax(state["hunger"]+ random.randint(5,10))
      new_state["energy"]= minmax(state["energy"]- random.randint(10,20))
@@ -33,7 +34,7 @@ def minmax(value):
      return new_state
   
 
-  def eat(state):
+def eat(state):
      new_state= dict(state)
      new_state["hunger"]= minmax(state["hunger"]- random.randint(5,10))
      new_state["energy"]= minmax(state["energy"]+ random.randint(10,20))
@@ -41,7 +42,7 @@ def minmax(value):
      new_state["health"]= minmax(state["health"]+ random.randint(0,5))
      return new_state
   
-  def sleep(state):
+def sleep(state):
      new_state= dict(state)
      new_state["hunger"]= minmax(state["hunger"]+ random.randint(5,10))
      new_state["energy"]= minmax(state["energy"]+ random.randint(10,20))
@@ -50,7 +51,7 @@ def minmax(value):
      return new_state
   
 
-  def pet(state):
+def pet(state):
      new_state= dict(state)
      new_state["hunger"]= minmax(state["hunger"]+ random.randint(5,10))
      new_state["energy"]= minmax(state["energy"]- random.randint(10,20))
@@ -59,7 +60,7 @@ def minmax(value):
      return new_state
   
 
-  def _check_status(state):
+def _check_status(state):
     # Limitamos los valores entre 0 y 100
     self.hunger = max(0, min(self.hunger, 100))
     self.energy = max(0, min(self.energy, 100))
@@ -70,8 +71,8 @@ def minmax(value):
       print(f"{self.name} has passed away.")
       exit()  # Salir del juego si la mascota muere
 
-
-    while True:
+ 
+while True:
         status()
         option = input("What do you want to do? (play, eat, sleep, pet, exit): ")
 
